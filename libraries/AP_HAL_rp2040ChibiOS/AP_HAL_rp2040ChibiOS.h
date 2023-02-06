bcc397e595d307c80f57694504e99ec0dfbf3e2c
Szilveszter Zsigmond <zsigmondszilveszter@yahoo.com>
2023-02-06 09:05:01 +0200

    rp2040 minor code cosmetics

--- libraries/AP_HAL_rp2040ChibiOS/AP_HAL_rp2040ChibiOS.h
+++ libraries/AP_HAL_rp2040ChibiOS/AP_HAL_rp2040ChibiOS.h
@@ -4,17 +4,17 @@
 #include <AP_HAL/AP_HAL.h>
 
 /**
- * Umbrella header for AP_HAL_Empty module.
+ * Umbrella header for AP_HAL_rp2040ChibiOS module.
  * The module header exports singleton instances which must conform the
  * AP_HAL::HAL interface. It may only expose implementation details (class
- * names, headers) via the Empty namespace.
- * The class implementing AP_HAL::HAL should be called HAL_Empty and exist
+ * names, headers) via the rp2040ChibiOS namespace.
+ * The class implementing AP_HAL::HAL should be called HAL_rp2040ChibiOS and exist
  * in the global namespace. There should be a single const instance of the
- * HAL_Empty class called AP_HAL_Empty, instantiated in the HAL_Empty_Class.cpp
- * and exported as `extern const HAL_Empty AP_HAL_Empty;` in HAL_Empty_Class.h
+ * HAL_rp2040ChibiOS class called AP_HAL_rp2040ChibiOS, instantiated in the HAL_rp2040ChibiOS_Class.cpp
+ * and exported as `extern const HAL_rp2040ChibiOS AP_HAL_rp2040ChibiOS;` in HAL_rp2040ChibiOS_Class.h
  *
  * All declaration and compilation should be guarded by CONFIG_HAL_BOARD macros.
- * In this case, we're using CONFIG_HAL_BOARD == HAL_BOARD_EMPTY.
+ * In this case, we're using CONFIG_HAL_BOARD == HAL_BOARD_RP2040CHIBIOS.
  * When creating a new HAL, declare a new HAL_BOARD_ in AP_HAL/AP_HAL_Boards.h
  */
 
