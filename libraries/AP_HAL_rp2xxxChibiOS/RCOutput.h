925fecc2ec0a381da6d6cb3b9e533832d9b66b46
Szilveszter Zsigmond <zsigmondszilveszter@yahoo.com>
2026-04-10 16:50:09 +0300

    WIP bidirectional dshot

--- libraries/AP_HAL_rp2xxxChibiOS/RCOutput.h
+++ libraries/AP_HAL_rp2xxxChibiOS/RCOutput.h
@@ -3,9 +3,16 @@
 #include "AP_HAL_rp2xxxChibiOS.h"
 
 #include "hal.h"
+#ifdef HAL_WITH_BIDIR_DSHOT
+#include <AP_ESC_Telem/AP_ESC_Telem.h>
+#endif
 #include "hwdef/common/pio.h"
 
-class Rp2xxxChibiOS::RCOutput : public AP_HAL::RCOutput {
+class Rp2xxxChibiOS::RCOutput : public AP_HAL::RCOutput
+#ifdef HAL_WITH_BIDIR_DSHOT
+  , AP_ESC_Telem_Backend
+#endif
+{
 public:
     void     init() override;
     void     set_freq(uint32_t chmask, uint16_t freq_hz) override;
@@ -23,7 +30,17 @@ public:
     void     timer_tick() override;
     void     set_dshot_rate(uint8_t dshot_rate, uint16_t loop_rate_hz) override;
     uint32_t get_dshot_period_us() const override { return _dshot_period_us; }
+#ifdef HAL_WITH_BIDIR_DSHOT
+    uint16_t get_erpm(uint8_t chan) const override { return chan < NUM_CHANNELS ? _bdshot_erpm[chan] : 0U; }
+    float    get_erpm_error_rate(uint8_t chan) const override;
+    bool     new_erpm() override { return _bdshot_update_mask != 0U; }
+    uint32_t read_erpm(uint16_t* erpm, uint8_t len) override;
+#endif
     void     set_telem_request_mask(uint32_t mask) override;
+    void     set_bidir_dshot_mask(uint32_t mask) override;
+#ifdef HAL_WITH_BIDIR_DSHOT
+    void     set_motor_poles(uint8_t poles) override { _bdshot_motor_poles = poles == 0U ? 1U : poles; }
+#endif
     void     set_dshot_esc_type(DshotEscType esc_type) override;
     DshotEscType get_dshot_esc_type() const override { return _dshot_esc_type; }
     bool     serial_setup_output(uint8_t chan, uint32_t baudrate, uint32_t chanmask) override;
@@ -50,11 +67,14 @@ private:
         PIO pio;
         uint8_t sm;
     };
+    static constexpr uint8_t NUM_PIO_PROGRAMS = 3;
     enum class PIOProgramType : uint8_t {
         None,
         OneShot,
         DShot,
     };
+    static constexpr uint16_t INVALID_ERPM = 0xFFFFU;
+    static constexpr uint16_t ZERO_ERPM = 0x0FFFU;
     ChanMap chan_map[NUM_CHANNELS] = {};
     PIOChanMap pio_chan_map[NUM_CHANNELS] = {};
 
@@ -66,9 +86,8 @@ private:
     output_mode _chan_mode[NUM_CHANNELS];   // MODE_PWM_NORMAL / ONESHOT / ONESHOT125
     uint8_t     _num_drivers = 0;
     uint32_t    _pio_mode_mask = 0;
-    uint8_t     _pio_offset[2] = {INVALID_PIO_OFFSET, INVALID_PIO_OFFSET};
+    uint8_t     _pio_offset[2][NUM_PIO_PROGRAMS] = {};
     uint8_t     _serial_io_offset[2] = {INVALID_PIO_OFFSET, INVALID_PIO_OFFSET};
-    PIOProgramType _pio_program_type[2] = {PIOProgramType::None, PIOProgramType::None};
     bool        _pio_ready[NUM_CHANNELS] = {};
     uint8_t     _dshot_rate = 0;
     uint8_t     _dshot_cycle = 0;
@@ -82,6 +101,15 @@ private:
     uint8_t     _serial_chan = INVALID_CHANNEL;
     uint32_t    _serial_chanmask = 0;
     uint32_t    _serial_baudrate = 0;
+#ifdef HAL_WITH_BIDIR_DSHOT
+    uint32_t    _bdshot_mask = 0;
+    uint16_t    _bdshot_erpm[NUM_CHANNELS] = {};
+    uint32_t    _bdshot_update_mask = 0;
+    uint16_t    _bdshot_erpm_errors[NUM_CHANNELS] = {};
+    uint16_t    _bdshot_erpm_clean_frames[NUM_CHANNELS] = {};
+    uint32_t    _bdshot_erpm_last_stats_ms[NUM_CHANNELS] = {};
+    uint8_t     _bdshot_motor_poles = 14;
+#endif
 
     PWMConfig pwm_cfg[NUM_CHANNELS];
     PWMDriver *pwm_drivers[NUM_CHANNELS] = {};
@@ -97,15 +125,24 @@ private:
     static bool _is_dshot_mode(output_mode mode);
     static uint32_t _dshot_bitrate(output_mode mode);
     uint32_t _dshot_pulse_time_us(uint8_t chan) const;
+    uint32_t _dshot_telem_time_us(uint8_t chan) const;
+    uint32_t _dshot_transaction_time_us(uint8_t chan) const;
     bool _can_send_dshot_pulse(uint8_t chan) const;
     uint32_t _dshot_channel_mask() const;
     void _signal_dshot_event(eventmask_t mask);
     static void dshot_update_tick(virtual_timer_t *vt, void *ctx);
     bool _uses_pio(uint8_t chan) const;
     bool _serial_suspended(uint8_t chan) const;
+    bool _load_output_program(PIO pio, PIOProgramType program_type, uint8_t &offset);
     bool _load_serial_program(PIO pio, uint8_t &offset);
     bool _init_serial_sm(void);
     void _release_serial_channel(uint8_t chan);
     bool _init_pio_channel(uint8_t chan);
     void _set_pin_mode(uint8_t chan);
+#ifdef HAL_WITH_BIDIR_DSHOT
+    bool _is_bidir_dshot(uint8_t chan) const;
+    void _drain_bidir_rx(uint8_t chan);
+    uint16_t _decode_bdshot_gcr(uint32_t raw_samples) const;
+    bool _decode_bdshot_erpm(uint16_t encodederpm, uint8_t chan);
+#endif
 };
--- libraries/AP_HAL_rp2xxxChibiOS/hwdef/common/dshot_output.pio.h
+++ libraries/AP_HAL_rp2xxxChibiOS/hwdef/common/dshot_output.pio.h
@@ -3,25 +3,39 @@
 #include "pio.h"
 
 #define dshot_output_wrap_target 0
-#define dshot_output_wrap 6
+#define dshot_output_wrap 17
 
 static const uint16_t dshot_output_pio_program_instructions[] = {
             //     .wrap_target
-            // 8 PIO ticks per DShot bit cell:
-            // bit 0 = 3 ticks high, 5 ticks low
-            // bit 1 = 6 ticks high, 2 ticks low
-    0b1000000010100000, //  0: pull   block                   side 0
-    0b1110000000101111, //  1: set    x, 15                  side 0
-    0b0110000001000001, //  2: out    y, 1                   side 0
-    0b0001001001100101, //  3: jmp    !y, 5              [2] side 1
-    0b0001001000000110, //  4: jmp    6                  [2] side 1
-    0b0000001000000110, //  5: jmp    6                  [2] side 0
-    0b0000000001000010, //  6: jmp    x--, 2                 side 0
+            // Transaction format in the TX FIFO:
+            //   word 0: [31:16] DShot packet, [15] bidir enable
+            //   word 1: (turnaround_loops - 1) for bidir transactions only
+            //
+            // The SM runs with 40 cycles per DShot TX bit. With that divider the
+            // same program can sample the bidir telemetry return every 32 cycles.
+    0x80A0, //  0: pull   block
+    0xE02F, //  1: set    x, 15
+    0x7741, //  2: out    y, 1               [7] side 1
+    0x1665, //  3: jmp    !y, 5              [6] side 1
+    0x1E06, //  4: jmp    6                 [14] side 1
+    0x0E06, //  5: jmp    6                 [14] side 0
+    0x0942, //  6: jmp    x--, 2             [9] side 0
+    0x6041, //  7: out    y, 1
+    0x0060, //  8: jmp    !y, 0
+    0x80A0, //  9: pull   block
+    0xA027, // 10: mov    x, osr
+    0xE080, // 11: set    pindirs, 0
+    0x0F4C, // 12: jmp    x--, 12           [15]
+    0xEF54, // 13: set    y, 20             [15]
+    0x4F01, // 14: in     pins, 1           [15]
+    0x0F8E, // 15: jmp    y--, 14           [15]
+    0x8020, // 16: push   block
+    0x0000, // 17: jmp    0
             //     .wrap
 };
 
 static const struct pio_program dshot_output_pio_program = {
     .instructions = dshot_output_pio_program_instructions,
-    .length = 7,
+    .length = 18,
     .origin = -1,
 };
